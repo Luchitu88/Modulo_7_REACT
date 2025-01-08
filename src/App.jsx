e5be9f86457de7b8c063b4a81e9f183a08dd47be
Luchitu88 <luis.iturricha@gmail.com>
2025-01-08 17:19:37 -0400

    Cambio de nombre de titulo

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Diplmado Full Stack Front End</h1>
+      <h2>Curso de React Modulo 7</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
