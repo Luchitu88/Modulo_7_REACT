de48fce032d647b013780bd12bbb8a9e2a8c12b7
Luchitu88 <luis.iturricha@gmail.com>
2025-01-09 21:49:55 -0400

    resolucion laboratorio 6

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,9 +5,12 @@ import OpenLink from '../components/Core/OpenLink.jsx'
 import viteLogo from "../assets/vite.svg";
 import reactLogo from "../assets/react.svg";
 import githubLogo from "../assets/github.svg";
+import { useSelector, useDispatch } from 'react-redux'
 const Home = () => {
+    const count = useSelector((state) => state.counter.value)
     return (
         <>
+         <span>{count}</span>
          <>
           <OpenLink 
             redirectURL="https://vite.dev" 
