cefce22a39acdcc3b71af6be5978fd862d6bb6c1
Luchitu88 <luis.iturricha@gmail.com>
2025-01-14 21:52:53 -0400

    Se agrega InitialValue en About

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,6 +1,11 @@
+import { useSelector } from "react-redux";
 const About = () => {
+    const count = useSelector((state) => state.product.initialValue)
     return (
-        <>
+        <> 
+            <p> Component About</p>
+            Valor de InitialValue es <span>{count}</span>
+
         </>
     )
 }
