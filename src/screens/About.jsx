17aa700a2b0186203b7fabfc631a243108ae984d
Luchitu88 <luis.iturricha@gmail.com>
2025-01-14 22:02:48 -0400

    Se agrega el HOOK en About

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,11 +1,15 @@
 import { useSelector } from "react-redux";
+import { useState } from "react";
 const About = () => {
     const count = useSelector((state) => state.product.initialValue)
+    const [nameComponent, setNameComponent] = useState('About')
     return (
         <> 
-            <p> Component About</p>
-            Valor de InitialValue es <span>{count}</span>
-
+            <p> Component: { nameComponent }</p>
+            <button onClick={() => {setNameComponent('Curso de React')}}>Press</button>
+            <p>
+            Valor InitialValue = <span>{count}</span>
+            </p>
         </>
     )
 }
