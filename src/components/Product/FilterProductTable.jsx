50e4b7c93f3b8f3ee6c7e9cc6c36ec24cf503ec0
Luchitu88 <luis.iturricha@gmail.com>
2025-01-17 12:39:17 -0400

    Se pasa PRODUCTS a STORE

--- src/components/Product/FilterProductTable.jsx
+++ src/components/Product/FilterProductTable.jsx
@@ -1,7 +1,7 @@
 import SearchBar from './SearchBar.jsx'
 import ProductTable from './ProductTable.jsx';
 import { useState } from 'react';
-import { useSelector } from "react-redux";
+import {PRODUCTS} from '../../store/features/product/productSlice.js';
  function  FilterableProductTable  ({ products })  {
   const [filterText, setFilterText] = useState('');
   const [inStockOnly, setInStockOnly] = useState(false);
@@ -19,16 +19,6 @@ import { useSelector } from "react-redux";
       </div>
     );
   }
-  
-  const PRODUCTS = [
-    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
-  ];
-  
  
   export default function FilterProductTable(){
     return <FilterableProductTable products={PRODUCTS} />;
--- src/store/features/product/productSlice.js
+++ src/store/features/product/productSlice.js
@@ -1,15 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
 const initialState = {
   initialValue: 10,
-  dataProduct: [
-    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
-  ]
+
 }
 
 export const productSlice = createSlice({
@@ -23,4 +25,7 @@ export const productSlice = createSlice({
 })
 export const { setInitialValue} = productSlice.actions
 
+export {PRODUCTS};
+
 export default productSlice.reducer
+
